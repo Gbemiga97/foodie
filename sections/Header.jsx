75e22937c09ce333a252880bc07bc0f6d801f1df
Gbemiga Salam <holuwagbemiga97@gmail.com>
2024-03-13 15:11:00 +0100

    initial commit

---
+++ sections/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <header className=" shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 py-6">
+        <div className="container">
+           <h1 className="text-4xl">header</h1>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../sections"
+
+const App = () => {
+  return (
+    <main className="max-w-[1400px] mx-auto overflow-hidden ">
+      <Header />
+    </main>
+  )
+}
+
+export default App
